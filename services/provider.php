de91e67f1168c7f05d185f53a97f8561aee1dd7a
bwbohl <b.w.bohl@gmail.com>
2026-03-25 00:20:01 +0100

    add services/provider.php

---
+++ services/provider.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * joomla-lastmodified
+ * @copyright Copyright (C) 2026 Benjamin W. Bohl <b.w.bohl@gmail.com>
+ * @license   GNU General Public License version 3 or later; see LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+defined('_JEXEC') or die;
+
+use Bwbohl\Plugin\Content\LastModified\Extension\LastModified;
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+return new class implements ServiceProviderInterface {
+	/**
+	 * Registers the plugin service with the DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.0
+	 */
+	public function register(Container $container): void
+	{
+		$container->set(
+			PluginInterface::class,
+			function (Container $container) {
+				$plugin = new LastModified(
+					$container->get(DispatcherInterface::class),
+					(array) PluginHelper::getPlugin('content', 'lastmodified')
+				);
+				$plugin->setApplication(Factory::getApplication());
+
+				return $plugin;
+			}
+		);
+	}
+};
